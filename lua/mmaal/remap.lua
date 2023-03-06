a7ae048ccc9d4928d01b72335f49deba4e0c972b
Mark M <mkmisdev@gmail.com>
2023-03-06 17:24:08 -0600

    Adding noremap options and adding explicit LSP config

--- lua/mmaal/remap.lua
+++ lua/mmaal/remap.lua
@@ -6,7 +6,7 @@ vim.g.mapleader = " "
 -- To quickly get into Netrw
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, opts)
 -- Do not yank with x
-vim.keymap.set("n", "x", "\"_x")
+vim.keymap.set("n", "x", "\"_x", opts)
 
 -- vim modes --
 -- normal_mode = "n"
@@ -19,45 +19,45 @@ vim.keymap.set("n", "x", "\"_x")
 -- Insert --
 -- Quick Exit --
 -- either jk or caps 
-vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("i", "jk", "<ESC>", opts)
 
 -- New Tab & Window Splitting
-vim.keymap.set("n", "te", ":tabedit<Return>", { silent = true })
-vim.keymap.set("n", "ss", ":split<Return><C-w>w", { silent = true })
-vim.keymap.set("n", "sv", ":vsplit<Return><C-w>w", { silent = true })
+vim.keymap.set("n", "te", ":tabedit<Return>", term_opts)
+vim.keymap.set("n", "ss", ":split<Return><C-w>w", term_opts)
+vim.keymap.set("n", "sv", ":vsplit<Return><C-w>w", term_opts)
 
 -- Navigate Buffers
-vim.keymap.set("n", "<S-l>", ":bnext<Return>")
-vim.keymap.set("n", "<S-h>", ":bprevious<Return>")
+vim.keymap.set("n", "<S-l>", ":bnext<Return>", opts)
+vim.keymap.set("n", "<S-h>", ":bprevious<Return>", opts)
 
 -- Move window
 -- Still thinking of the best keybindings for these
 -- Up
-vim.keymap.set("n", "s<up>", "<C-w>k")
-vim.keymap.set("n", "sk", "<C-w>k")
+vim.keymap.set("n", "s<up>", "<C-w>k", opts)
+vim.keymap.set("n", "sk", "<C-w>k", opts)
 -- Down
-vim.keymap.set("n", "s<down>", "<C-w>j")
-vim.keymap.set("n", "sj", "<C-w>j")
+vim.keymap.set("n", "s<down>", "<C-w>j", opts)
+vim.keymap.set("n", "sj", "<C-w>j", opts)
 -- Left
-vim.keymap.set("n", "s<left>", "<C-w>h")
-vim.keymap.set("n", "sh", "<C-w>h")
+vim.keymap.set("n", "s<left>", "<C-w>h", opts)
+vim.keymap.set("n", "sh", "<C-w>h", opts)
 -- Right
-vim.keymap.set("n", "s<right>", "<C-w>l")
-vim.keymap.set("n", "sl", "<C-w>l")
+vim.keymap.set("n", "s<right>", "<C-w>l", opts)
+vim.keymap.set("n", "sl", "<C-w>l", opts)
 
 -- Visual --
 -- Stay in indent mode
-vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", "<", "<gv", opts)
 -- Move text up and down 
-vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", ">", ">gv", opts)
 
 -- Move text around and auto indent 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
 
 -- Stop yank filling buffer
-vim.keymap.set("v", "p", "\"_dP")
+vim.keymap.set("v", "p", "\"_dP", opts)
 
 -- Keep search terms in the middle
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "n", "nzzzv", opts)
+vim.keymap.set("n", "N", "Nzzzv", opts)
