efc66012bf44f1d9b1ca99765475a435d74686d7
Mark Maalouf <mkmisdev@gmail.com>
2025-01-06 21:34:31 -0600

    ensuring lsp servers/treesitter is set up for commonly used langs

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = { "lua", "javascript", "go" },
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            ensure_installed = { "lua", "javascript", "go", "python", "typescript" },
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end,
 }
