582080e77bb3b9684ee7f9100da22e35866e9092
Mark Maalouf <mkmisdev@gmail.com>
2025-01-07 22:47:42 -0600

    use prettierd

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -9,7 +9,7 @@ return {
 				null_ls.builtins.formatting.gofumpt,
 				null_ls.builtins.diagnostics.golangci_lint,
 				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.prettierd,
 			},
 			on_attach = function(client, bufnr)
 				if client.supports_method("textDocument/formatting") then
