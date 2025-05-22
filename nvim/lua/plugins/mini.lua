05bc94d68e46096d709718107afa201b7f5b8179
Mark Maalouf <mkmisdev@gmail.com>
2025-05-22 01:13:52 -0500

    add indent scopes and auto pair completion

---
+++ nvim/lua/plugins/mini.lua
@@ -0,0 +1,48 @@
+return {
+    {
+        "echasnovski/mini.indentscope",
+        version = false,
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            symbol = "│",
+            options = { try_as_border = true },
+        },
+        init = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = {
+                    "help",
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                    "Trouble",
+                    "lazy",
+                    "mason",
+                    "notify",
+                    "toggleterm",
+                    "lazyterm",
+                },
+                callback = function()
+                    vim.b.miniindentscope_disable = true
+                end,
+            })
+        end,
+    },
+    {
+        "echasnovski/mini.pairs",
+        opts = {
+            modes = { insert = true, command = true, terminal = false },
+            -- skip autopair when next character is one of these
+            skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+            -- skip autopair when the cursor is inside these treesitter nodes
+            skip_ts = { "string" },
+            -- skip autopair when next character is closing pair
+            -- and there are more closing pairs than opening pairs
+            skip_unbalanced = true,
+            -- better deal with markdown code blocks
+            markdown = true,
+            mappings = {
+                ["`"] = false,
+            },
+        },
+    },
+}
