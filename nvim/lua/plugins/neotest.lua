a7600bb4a8c7626efbf3b33045aecfa45e532950
Mark Maalouf <mkmisdev@gmail.com>
2025-05-23 19:47:54 -0500

    adding neotest support for JS/TS and go

---
+++ nvim/lua/plugins/neotest.lua
@@ -0,0 +1,75 @@
+return {
+    "nvim-neotest/neotest",
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-neotest/neotest-jest",
+        "nvim-neotest/neotest-python",
+        "nvim-neotest/neotest-vim-test",
+        { "fredrikaverpil/neotest-golang", version = "*" },
+    },
+
+    keys = {
+        { "<leader>ta", "<cmd>lua require('neotest').run.attach()<cr>",   desc = "Attach to the nearest test" },
+        { "<leader>tl", "<cmd>lua require('neotest').run.run_last()<cr>", desc = "Toggle Test Summary" },
+        {
+            "<leader>to",
+            "<cmd>lua require('neotest').output_panel.toggle()<cr>",
+            desc = "Toggle Test Output Panel",
+        },
+        { "<leader>tp", "<cmd>lua require('neotest').run.stop()<cr>",       desc = "Stop the nearest test" },
+        { "<leader>ts", "<cmd>lua require('neotest').summary.toggle()<cr>", desc = "Toggle Test Summary" },
+        { "<leader>tt", "<cmd>lua require('neotest').run.run()<cr>",        desc = "Run the nearest test" },
+        {
+            "<leader>tT",
+            "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>",
+            desc = "Run test the current file",
+        },
+        {
+            "<leader>td",
+            function()
+                require("neotest").run.run({ suite = false, strategy = "dap" })
+            end,
+            desc = "Debug nearest test",
+        },
+    },
+    config = function()
+        -- get neotest namespace (api call creates or returns namespace)
+        local neotest_ns = vim.api.nvim_create_namespace("neotest")
+        vim.diagnostic.config({
+            virtual_text = {
+                format = function(diagnostic)
+                    local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+                    return message
+                end,
+            },
+        }, neotest_ns)
+        require("neotest").setup({
+            adapters = {
+                require("neotest-jest")({
+                    jestCommand = "npm test --",
+                    jestConfigFile = "custom.jest.config.ts",
+                    env = { CI = true },
+                    cwd = function(path)
+                        return vim.fn.getcwd()
+                    end,
+                }),
+                require("neotest-vim-test")({
+                    ignore_file_types = { "python", "vim", "lua", "js", "ts" },
+                }),
+                require("neotest-python"),
+                require("neotest-golang")({
+                    runner = "gotestsum",
+                    go_test_args = {
+                        "-v",
+                        "-count=1",
+                        "-coverprofile=" .. vim.fn.getcwd() .. "/coverage.out",
+                    },
+                }),
+            },
+        })
+    end,
+}
